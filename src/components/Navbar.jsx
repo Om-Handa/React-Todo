bd9d9b820d69b940ee0a58bff4416d635937d8ce
Om-handa <omhanda246@gmail.com>
2025-06-21 15:45:20 +0530

    Updated commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -4,7 +4,7 @@ function Navbar() {
   return (
     <div>
       <nav className='w-full flex justify-center items-center h-16 bg-[#0B3948] text-white'>
-            <h1 className="text-4xl font-bold ">OM TODO APP</h1>
+            <h1 className="text-4xl font-bold ">TodoAppByOM</h1>
       </nav>
     </div>
   )
