d4210114b3806f1ceca26b2228b3c36b6bb1b856
Om-handa <omhanda246@gmail.com>
2025-06-21 16:06:47 +0530

    Updated Content

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,7 @@ function App() {
   const [todo, settodo] = useState("")
   const [todos, settodos] = useState([])
   const [showfinished, setshowfinished] = useState(true)
-  
+
   useEffect(() => {
     let todostring = localStorage.getItem("todos")
     if (todostring) {
@@ -15,7 +15,7 @@ function App() {
       settodos(todos)
     }
   }, [])
-  
+
   useEffect(() => {
     localStorage.setItem("todos", JSON.stringify(todos))
   }, [todos])
@@ -65,14 +65,13 @@ function App() {
   return (
     <>
       <Navbar />
-
       <div className=" mx-5 md:mx-0 flex justify-center items-center my-10">
         <div className='container md:w-1/2 w-full '>
           <div className="bg-[#0B3948] min-h-24 flex flex-col items-center space-y-6 p-2">
             <p className='text-4xl font-bold text-white'>Add your Todo</p>
             <div className="input w-full flex space-x-4 justify-center items-center">
-              <input type="text" className='rounded-lg w-3/4 p-1 h-9' value={todo} onChange={handleInput} placeholder='Enter Your Todo' />
-              <button disabled={todo.length < 4} className='bg-red-900 flex justify-center items-center' onClick={handleClick}>
+              <input type="text" className='rounded-lg w-3/4 p-1 h-9' onKeyDown={(e) => { if (e.key === 'Enter') {handleClick()}}} value={todo} onChange={handleInput} placeholder='Enter Your Todo' />
+              <button disabled={todo.length === 0} className='bg-red-900 flex justify-center items-center' onClick={handleClick}>
                 <span className="material-icons mx-2 my-1 font-bold text-3xl">add</span>
               </button>
             </div>
@@ -83,18 +82,18 @@ function App() {
               <h1 className='text-xl font-bold text-white'>Show Finished Todos?</h1>
             </div>
             {todos.length === 0 ? (<p className="text-white text-2xl font-bold mt-5">NO TODOS TO SHOW</p>) :
-            todos.map(items => {
-              return (!showfinished || !items.isCompleted) && <div key={items.id} className="todos border-4 p-3 border-slate-950 flex items-center w-full min-h-9 space-x-2">
-                <input type="checkbox" checked={items.isCompleted} className='w-16 h-7 accent-red-900' onChange={(e) => { checkChange(e, items.id) }} />
-                <div className={`w-4/5 text-center text-2xl font-bold ${items.isCompleted ? 'line-through' : ''}`}>{items.todo}</div>
-                <button onClick={(e) => { handleEdit(e, items.id) }} className="edit bg-red-900 max-h-9">
-                  <span className="material-icons p-2 ">edit</span>
-                </button>
-                <button onClick={(e) => { handleDelete(e, items.id) }} className="delete bg-red-900 max-h-9">
-                  <span className="material-icons p-2 ">delete</span>
-                </button>
-              </div>
-            })}
+              todos.map(items => {
+                return (!showfinished || !items.isCompleted) && <div key={items.id} className="todos border-4 p-3 border-slate-950 flex items-center w-full min-h-9 space-x-2">
+                  <input type="checkbox" checked={items.isCompleted} className='w-16 h-7 accent-red-900' onChange={(e) => { checkChange(e, items.id) }} />
+                  <div className={`w-4/5 text-center text-2xl font-bold ${items.isCompleted ? 'line-through' : ''}`}>{items.todo}</div>
+                  <button onClick={(e) => { handleEdit(e, items.id) }} className="edit bg-red-900 max-h-9">
+                    <span className="material-icons p-2 ">edit</span>
+                  </button>
+                  <button onClick={(e) => { handleDelete(e, items.id) }} className="delete bg-red-900 max-h-9">
+                    <span className="material-icons p-2 ">delete</span>
+                  </button>
+                </div>
+              })}
           </div>
         </div>
       </div>
